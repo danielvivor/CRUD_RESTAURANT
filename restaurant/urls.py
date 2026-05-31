bac936bee12a402660383109d79439b98979bd0b
Daniel Vivor <you@example.com>
2026-05-31 20:51:12 +0200

    feat(urls): register endpoint routing for create-booking path
    - Append create_booking endpoint path to app routing urlpatterns array.

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home_page, name='home'),
     path('add-review/', views.add_review, name='add_review'),
+    path('create-booking/', views.create_booking, name='create_booking'), # New path
 ]
