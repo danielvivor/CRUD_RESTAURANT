cf1c64ba6c77106105042a61582f027c186845e9
Daniel Vivor <you@example.com>
2026-06-05 03:36:46 +0200

    Reverse DEBUG setting to "True".

--- nala_project/settings.py
+++ nala_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5*jbw4)b6anhhc*hti2gx1x&r#s1!9bk45cg7x7qvh&26f&*_n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
