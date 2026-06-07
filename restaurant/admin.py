61842d93293d834a6cbdd30a3eccb833bc59f8e3
Daniel Vivor <you@example.com>
2026-06-07 11:49:49 +0200

    Register Reservation and Review models in admin interface.

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Reservation, Review
+
+# Register models to appear in the Admin Panel
+admin.site.register(Reservation)
+admin.site.register(Review)
